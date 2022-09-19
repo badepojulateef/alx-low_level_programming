74a74a1f1d629bf1075f996a37f437871ba4a11a
Adepoju Lateef <badepojulateef@gmail.com>
2022-09-19 18:53:22 +0100

    added task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ *
+ * @s: string to be reversed
+ *
+ */
+void rev_string(char *s)
+{
+	int l = strlen(str);
+	while (l >= 0)
+	{
+		_putchar(str[l])
+		l--;
+	}
+}
