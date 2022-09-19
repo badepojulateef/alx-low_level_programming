34dba816f1c3a065de2e409256553718987be8c3
Adepoju Lateef <badepojulateef@gmail.com>
2022-09-19 17:25:58 +0100

    modified the return value

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,8 +4,10 @@
  * _strlen - Function that returns the length the of a string
  * @s: char parameter
  * Return: integer string of a string
- */
+ */i
 int _strlen(char *s)
 {
-	return strlen(s);
+	int length;
+	length = strlen(s);
+	return length;
 }
