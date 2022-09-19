8e1c48d877b3fd805b50f163bf58c099e63080df
Adepoju Lateef <badepojulateef@gmail.com>
2022-09-19 18:58:08 +0100

    modified 5-rev_string.c again 2

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,10 +8,23 @@
  */
 void rev_string(char *s)
 {
-	int l = strlen(s);
-	while (l >= 0)
+	int length, j, i;
+	char v1, v2;
+
+	for (length = 0; s[length] != '\0'; length++)
 	{
-		_putchar(s[l]);
-		l--;
+	}
+
+	j = length - 1;
+	i = 0;
+
+	while (j > i)
+	{
+		v1 = s[i];
+		v2 = s[j];
+		s[i] = v2;
+		s[j] = v1;
+		j--;
+		i++;
 	}
 }
