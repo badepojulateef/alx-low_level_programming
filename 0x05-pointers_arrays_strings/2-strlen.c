79a4d79f9d6decc546ae4e48e0485e243cf5360c
Adepoju Lateef <badepojulateef@gmail.com>
2022-09-19 18:12:58 +0100

    modified betty

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strlen - Function that returns the length the of a string
+ * _strlen - function that returns the length the of a string
  * @s: char parameter
  * Return: integer string of a string
  */
