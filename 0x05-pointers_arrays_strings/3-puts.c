db2bd57b8e41f8acb915d0153e16d9fc18ce18c8
Adepoju Lateef <badepojulateef@gmail.com>
2022-09-19 18:36:08 +0100

    added task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string, followed by a new line, to stdout
+ * @str: string parameter
+ * Return: void
+ */
+void _puts()
+{
+puts(str);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,11 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
 #include <string.h>
 #include <stdio.h>
+
+
 
 void(reset_to_98(int *n));
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void puts(char *s);
