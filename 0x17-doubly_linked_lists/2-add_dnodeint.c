1268a8fa5707fa35ada297a2ef1e331480cfffe4
Adepoju Lateef <badepojulateef@gmail.com>
2022-12-09 05:07:35 +0100

    modified task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * add_dnodeint - adds a new node at the beginning
+ * add_dnodeint - adds a new_node at the beginning
  * of a dlistint_t list
  *
  * @head: head of the list
