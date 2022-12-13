1b2a5ab82cad602bd3ac21b9e9595794e1be4830
Adepoju Lateef <badepojulateef@gmail.com>
2022-12-13 04:01:50 +0100

    modified functions referencing

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -c -fpic functions/*.c
+gcc -Wall -Werror -Wextra -pedantic -c -fpic *.c
 gcc -shared -o liball.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
