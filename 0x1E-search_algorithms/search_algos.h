d76b1b1ceab5777099a4540cc1f80de8def02323
Adepoju Lateef <badepojulateef@gmail.com>
2023-04-14 02:40:28 +0100

    attempted task 7

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,9 +3,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int binary_search_recursive(int *array, int value, size_t left, size_t right);
+int jump_search(int *array, size_t size, int value);
 
 #endif /* SEARCH_ALGOS_H */
