1d2643c5ea912889049924709663016d4306393a
Adepoju Lateef <badepojulateef@gmail.com>
2023-04-14 03:32:41 +0100

    attempted task 9

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,42 @@
+#include "search_algos.h"
+
+/**
+ * interpolation_search - searches for a value in a sorted array
+ * of integers using the Interpolation search algorithm
+ *
+ * @array: array to be searched
+ * @size: size of the array
+ * @value: value to be searched for
+ *
+ * Return: int
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t pos;
+	size_t l;
+	size_t r;
+	size_t tmp;
+
+	if (array == NULL || size == 0)
+		return (-1);
+
+	l = 0;
+	r = size - 1;
+
+	while (l <= r && value >= array[l] && value <= array[r])
+	{
+		tmp = (((r - l) / (array[r] - array[l])) * (value - array[l]));
+		pos = l + tmp;
+		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+
+		if (array[pos] == value)
+			return (pos);
+		else if (array[pos] < value)
+			l = pos + 1;
+		else
+			r = pos - 1;
+	}
+
+	printf("Value checked array[%lu] is out of range\n", pos);
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -9,5 +9,6 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int binary_search_recursive(int *array, int value, size_t left, size_t right);
 int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
 
 #endif /* SEARCH_ALGOS_H */
